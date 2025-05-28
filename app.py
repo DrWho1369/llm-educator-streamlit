3adafd402beebe500e441967285410e8e1706757
Tom Baker <125075858+DrWho1369@users.noreply.github.com>
2025-05-28 14:26:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import requests
-from pdf_summarizer import summarize_uploaded_pdf
+from pdf_simmariser import summarize_uploaded_pdf
 
 LLM_API_URL = st.secrets["LLM_API_URL"]
 
