39d927e6dc838945f708c3f2e72c33dbf2fb534a
Tom Baker <125075858+DrWho1369@users.noreply.github.com>
2025-06-04 23:34:04 +0100

    Update hunspell.py

--- pages/hunspell.py
+++ pages/hunspell.py
@@ -1,18 +1,18 @@
-import streamlit as st
-from hunspell import Hunspell
+# import streamlit as st
+# from hunspell import Hunspell
 
-h = Hunspell()
+# h = Hunspell()
 
-st.title("Hunspell Spellchecker Demo")
-user_input = st.text_area("Enter text:", height=150)
-if st.button("Spellcheck"):
-    words = user_input.split()
-    corrected = []
-    for word in words:
-        if not h.spell(word):
-            suggestions = h.suggest(word)
-            corrected.append(suggestions[0] if suggestions else word)
-        else:
-            corrected.append(word)
-    st.subheader("Corrected Text")
-    st.write(' '.join(corrected))
+# st.title("Hunspell Spellchecker Demo")
+# user_input = st.text_area("Enter text:", height=150)
+# if st.button("Spellcheck"):
+#     words = user_input.split()
+#     corrected = []
+#     for word in words:
+#         if not h.spell(word):
+#             suggestions = h.suggest(word)
+#             corrected.append(suggestions[0] if suggestions else word)
+#         else:
+#             corrected.append(word)
+#     st.subheader("Corrected Text")
+#     st.write(' '.join(corrected))
